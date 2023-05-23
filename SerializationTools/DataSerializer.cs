a0d5f0bb3fbaa5c02412b065a055cf89bd5097a7
Kolya <kolyaklimk@gmail.com>
2023-05-23 16:52:48 +0300

    Add Xml

--- SerializationTools/DataSerializer.cs
+++ SerializationTools/DataSerializer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using System.Xml.Serialization;
 
 namespace SerializationTools;
 
@@ -20,5 +21,23 @@ public static class DataSerializer
     public static void JsonDeserializeAsync<T>(Stream utf8Json)
     {
         JsonSerializer.DeserializeAsync<T>(utf8Json);
+    }
+    public static string XmlSerialize<T>(T data)
+    {
+        var serializer = new XmlSerializer(typeof(T));
+        using(TextWriter writer = new StringWriter())
+        {
+            serializer.Serialize(writer, data);
+            return writer.ToString();
+        }
+    }
+
+    public static T XmlDeserialize<T>(string xmlString)
+    {
+        var serializer = new XmlSerializer(typeof(T));
+        using(var reader = new StringReader(xmlString))
+        {
+            return (T)serializer.Deserialize(reader);
+        }
     }
 }
