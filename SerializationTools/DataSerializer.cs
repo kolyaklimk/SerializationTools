2166efb0123bda2e5e36ece8f299a3aad42037d3
Kolya <kolyaklimk@gmail.com>
2023-05-23 16:52:08 +0300

    Add Json methods

--- SerializationTools/DataSerializer.cs
+++ SerializationTools/DataSerializer.cs
@@ -1,5 +1,24 @@
-﻿namespace SerializationTools;
+﻿using System.Text.Json;
+
+namespace SerializationTools;
 
 public static class DataSerializer
 {
+    public static string JsonSerialize(object data)
+    {
+        return JsonSerializer.Serialize(data);
+    }
+    public static T JsonDeserialize<T>(string jsonString)
+    {
+        return JsonSerializer.Deserialize<T>(jsonString);
+    }
+
+    public static void JsonSerializeAsync<T>(Stream utf8Json, T value)
+    {
+        JsonSerializer.SerializeAsync(utf8Json, value);
+    }
+    public static void JsonDeserializeAsync<T>(Stream utf8Json)
+    {
+        JsonSerializer.DeserializeAsync<T>(utf8Json);
+    }
 }
